fa639bbb5d2d2e6e6bbb07656d3b04b874c2e6e7
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-01 18:30:14 -0700

    handled signup post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
-
+ 
 import { ROOT_URL } from '../config';
 
 export function signUp(fields, success) {
-    console.log(fields);
-    return function (dispatch) {
-        // axios.post(`${ROOT_URL}/signUp`)
-        //     .then(response => {
-        //         //do something with response
-        //     })
-        //     .catch(err => {
-        //         if (err) { console.log(err) }
-        //     })
-    }
-}
+   console.log(fields);
+   return function(dispatch) {
+       axios.post(`${ROOT_URL}/signUp`, fields)
+           .then(response => {
+              console.log(response);
+              success()
+           })
+           .catch(err => {
+               if(err) { console.log(err) }
+           })
+   }
+} 
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 import SignupForm from './signupForm';
 
 class Signup extends Component {
 
     onSubmit = (fields) => {
-        
+
+        this.props.signUp(fields, () => {
+            console.log('navigate to dashboard');
+            this.props.history.push('/dashboard');
+        })
     }
 
     render() {
-        return (
-            <div className='sign-up'>
-                <SignupForm onSubmit={(event) => this.onSubmit(event)} />
-            </div>
-        )
+         return (
+             <div className='sign-up'>
+                 <SignupForm onSubmit={(event) => this.onSubmit(event)}/>
+             </div>
+         )
     }
 }
 
-export default Signup;
+export default connect(null, actions)(Signup); 
