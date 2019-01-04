045f75bb491758f0607812d6dde92cfbce34066f
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-03 23:22:55 -0700

    parsed date for archive item component

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -4,7 +4,13 @@ function ArchiveItem({title, date}) {
     return (
         <div className='archive-item'>
             <div className='archive-item__title'>{title}</div>
-            <div className='achive-item__date'>{date}</div>
+            <div className='achive-item__date'>
+                { date.getMonth() + 1 }
+                /
+                { date.getDate() }
+                /
+                { date.getFullYear() - 2000 }
+            </div>
         </div>
     )
 }
@@ -15,9 +21,11 @@ class NewsletterArchive extends Component {
             <div className='newsletter-archive'>
                 <div className='newsletter-archive__title'>Archive</div>
                 <div className='newsletter-archive__items archive-items'>
-                    <ArchiveItem title='hey' date='09/16/1995'/>
-                    <ArchiveItem title='hey' date='this is the date'/>
-                    <ArchiveItem title='hey' date='this is the date'/>
+                    <ArchiveItem title='hey' date={new Date()}/>
+                    <ArchiveItem title='hey' date={new Date()}/>
+                    <ArchiveItem title='hey' date={new Date()}/>
+                    <ArchiveItem title='hey' date={new Date()}/>
+                    <ArchiveItem title='hey' date={new Date()}/>
                 </div>
             </div>
         )
