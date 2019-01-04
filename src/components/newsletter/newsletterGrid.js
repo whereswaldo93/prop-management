1bba1a9bfb5dbc2b3afeede53e55fade44c8322b
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-03 20:28:20 -0700

    styled the newsletter-box component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import NewsletterBox from './newsletterBox';
+
 class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
-            hello!
+                <NewsletterBox date={new Date()}/>
             </div>
         );
     }
