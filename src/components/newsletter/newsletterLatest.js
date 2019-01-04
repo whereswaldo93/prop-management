9f0a974c101c372a2c2489ab1775d00ffaa4ce2a
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-04 11:45:51 -0700

    built a button component

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 
+function Button({className, callback, text, icon}) {
+    if(icon) {
+        return (
+            <a onClick={callback} className={`${className} button`}>
+                <i className={icon}></i>
+            </a>
+        )
+    }
+
+}
+
 class NewsletterLatest extends Component {
+
+    handleEdit = () => {
+        console.log('trying to handle edit')
+    }
+
     render() {
         const { title, imageURL, body } = this.props;
         return (
-            <div className='newsletter-latest'>
+            <div className='newslette}r-latest'>
                 <h1 className='newsletter-latest__title'>{title}</h1>
                 <img className='newsletter-latest__image' src={imageURL}/>
+                <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon="fas fa-pencil-alt"/>
                 <div className='newsletter-latest__body'>
                     <p>{body}</p>
                 </div>
