ee6e0e67087891dce7393d2ca5f51a6d3b612dd6
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-05 12:41:46 -0700

    created and placed add newsletter button on newsletter grid

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-
 import NewsletterBox from './newsletterBox';
 import NewsletterArchive from './newsletterArchive';
 import NewsletterLatest from './newsletterLatest';
+import Button from '../button';
 
 class NewsletterGrid extends Component {
+
+    handleAddNewsletter = () => {
+        this.props.history.push('/newsletter/new');
+    }
+
     render() {
 
         const latest = {
             _id: '115',
-            title: 'happy holidays',
-            body: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            title: 'Happy Holidays Fam',
+            body: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
             date: new Date(),
-            imageURL: 'http://via.placeholder.com/960x258'
+            imageUrl: 'http://via.placeholder.com/960x258'
         }
 
         return (
             <div className='newsletter-grid'>
+                <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()}/>
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
-                <NewsletterLatest { ...latest }/>
+                <NewsletterLatest {...latest}/>
             </div>
-        );
+        )
     }
 }
-
 export default NewsletterGrid;
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -1,35 +1,24 @@
 import React, { Component } from 'react';
-
-function Button({className, callback, text, icon}) {
-    if(icon) {
-        return (
-            <a onClick={callback} className={`${className} button`}>
-                <i className={icon}></i>
-            </a>
-        )
-    }
-
-}
+import Button from '../button';
 
 class NewsletterLatest extends Component {
 
     handleEdit = () => {
-        console.log('trying to handle edit')
+        console.log('trying to handle edit');
     }
 
     render() {
-        const { title, imageURL, body } = this.props;
+        const { title, imageUrl, body } = this.props;
         return (
-            <div className='newslette}r-latest'>
+            <div className='newsletter-latest'>
                 <h1 className='newsletter-latest__title'>{title}</h1>
-                <img className='newsletter-latest__image' src={imageURL}/>
-                <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon="fas fa-pencil-alt"/>
+                <img className='newsletter-latest__image' src={imageUrl}/>
+                <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon='fas fa-pencil-alt'/>
                 <div className='newsletter-latest__body'>
                     <p>{body}</p>
                 </div>
             </div>
-        );
+        )
     }
 }
-
 export default NewsletterLatest;
