5e9d9c7ea3d80ace21027be52d82ad98de7b806e
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-06 20:54:51 -0700

    built cancel button and placed form buttons on grid

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -5,13 +5,24 @@ import NewNewsletterForm from './newsletterNewForm';
 class NewNewsletter extends Component {
 
     onSubmit = (fields) => {
-        console.log('trying to handle submit')
+        // console.log(button)
+        // if(button =='submit') {
+        //     // save newsletter on backend. perform a post request here.
+        //     console.log('trying to submit to backend');
+        // } 
+        // this.props.history.push('/dasboard');
+        console.log('trying to submit');
+    }
+
+    onCancel = () => {
+        // this.props.history.push('/dasboard');
+        console.log('trying to cancel');
     }
 
     render() {
         return (
             <div className='new-newsletter'>
-                <NewNewsletterForm onSubmit ={(event) => this.onSubmit(event)}/>
+                <NewNewsletterForm onCancel={() => this.onCancel()} onSubmit ={(event) => this.onSubmit(event)}/>
             </div>
         );
     }
--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -33,13 +33,28 @@ class NewNewsletterForm extends Component {
                     component={FormTextArea} 
                 />
                 <Field 
-                    className="sign-in-form__submit" 
+                    className="new-newsletter-form__submit" 
                     small={true}
                     danger={true}
                     name="submit" 
                     type="submit" 
                     title="Submit"
                     component={FormButton} 
+                    onClick={handleSubmit(values => {
+                        this.props.onSubmit({
+                            ...values,
+                            button: 'button'
+                        })
+                    })}
+                />
+                <Field 
+                    className="new-newsletter-form__cancel" 
+                    small={true}
+                    name="cancel" 
+                    type="button" 
+                    title="Cancel"
+                    component={FormButton} 
+                    onClick={this.props.onCancel}
                 />
             </form>
         )
