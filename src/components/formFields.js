d4277ec1cf241752a24e7de01052579868d4d682
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-06 21:50:52 -0700

    form image styles

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -65,10 +65,9 @@ export class FormImage extends Component {
                 <label className="form-image__title">{title}</label>
                 <img 
                     className= 'form-image__image'
-                    type={type}
-                    {...input}
                     src={imageUrl}
                 />
+                <input {...input} type={type} />
             </div>
         )
     }
