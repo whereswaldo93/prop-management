ba835c960b16e10466a46523bfe29daeab46573c
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-11 13:31:53 -0700

    refactored newsletternewform to take in custom data, and provided newsletterEdit.js component with field one and two titles

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -9,6 +9,10 @@ class NewNewsletterForm extends Component {
     render() {
 
         const {handleSubmit, formTitle, newsletterToEdit } = this.props;
+        const {
+            fieldOnePlaceholder, fieldOneTitle,
+            fieldTwoPlaceholder, fieldTwoTitle
+        } = this.props;
 
         var title = null;
         var body = null;
@@ -27,19 +31,19 @@ class NewNewsletterForm extends Component {
                 />
                 <Field 
                     className="new-newsletter-form__newsletter-title" 
-                    placeholder ='Newsletter Title' 
+                    placeholder ={fieldOnePlaceholder} 
                     name="title" 
                     type="text" 
-                    title="Newsletter Title"
+                    title={fieldOneTitle}
                     component={FormInput} 
                     editValue={title ? title : null}
                 />
                 <Field 
                     className="new-newsletter-form__body" 
-                    placeholder ='Newsletter Body' 
+                    placeholder ={fieldTwoPlaceholder} 
                     name="body" 
                     type="text" 
-                    title="Body"
+                    title={fieldTwoTitle}
                     component={FormTextArea}
                     editValue={body ? body : null} 
                 />
